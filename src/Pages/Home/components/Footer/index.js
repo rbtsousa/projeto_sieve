2891ee254f622212c4660843df1110856b6b217b
Luciannodev <joseluciano.dev@gmail.com>
2019-12-24 22:54:31 -0300

    build: Escopo do footer.

--- src/Pages/Home/components/Footer/index.js
+++ src/Pages/Home/components/Footer/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import {StyledFooter,Rodape} from './styles'
-
+import { StyledFooter, Rodape, StyledIcons,HR} from './styles'
+import { AiOutlineFacebook, AiOutlineInstagram } from 'react-icons/ai'
+import { MdPhoneInTalk } from 'react-icons/md'
 export default function Footer() {
   return (
     <>
-    <StyledFooter>
-        <div><img alt=""/><span></span></div>
-        <div><img alt=""/><img alt= ""/></div>
-        <hr></hr>
+      <StyledFooter>
+        <StyledIcons>
+          <div><MdPhoneInTalk color="#fff" size="55px" /><span>21 2687-7214</span></div>
+          <div><AiOutlineFacebook color="#fff" size="55px" /><AiOutlineInstagram color="#fff" size="56px" /></div>
+        </StyledIcons>
+        <HR></HR>
         <Rodape>
-        <div></div>
-        <hr></hr>
-        <div></div>
-        <hr></hr>
-        <div></div>
+          <div>
+            <span>Projeto</span>
+            <ul>
+              <li>Quem Somos</li>
+              <li>Politica de Privacidade</li>
+              <li>Como funciona</li>
+            </ul>
+          </div>
+          <hr></hr>
+          <div>
+            <span>Contato</span>
+            <ul>
+              <li>Fale Conosco</li>
+              <li>Indique para um amigo</li>
+              <li>Perguntas frequentes</li>
+            </ul>
+          </div>
+          <hr></hr>
+          <div>
+            <span>Projeto</span>
+            <ul>
+              <li>Quem Somos</li>
+              <li>Trabalhe Conosco</li>
+            </ul>
+          </div>
         </Rodape>
-    </StyledFooter>
+      </StyledFooter>
     </>
   );
 }
--- src/Pages/Home/components/Footer/styles.js
+++ src/Pages/Home/components/Footer/styles.js
@@ -1,7 +1,49 @@
 import styled from 'styled-components'
 
 export const StyledFooter = styled.div`
+    margin-top: 50px;
+    background-color: #37494D;
+    
 `;
 
 export const Rodape = styled.div`
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+  span{
+        font-size: 18px;
+        color: #fff;
+        font-weight: bolder;
+    };
+    hr{
+        margin-top: 10px;
+        height: 60px;
+    }
+    ul{
+        margin: 15px;
+    }
+    span{
+        margin: 10px;
+    }
+`;
+
+export const StyledIcons = styled.div`
+    display:flex;
+    justify-content: space-between;
+    div{
+        display: flex;
+        align-items: center;
+    }
+    ;
+    span{
+        font-size: 25px;
+        color: #fff;
+        font-weight: bolder;
+    }
+`
+export const HR = styled.hr`
+    margin: 0 auto;
+    width: 70%;
+
 `
